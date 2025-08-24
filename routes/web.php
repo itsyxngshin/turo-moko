ae98f7d84d97615443aeaea015e0c9b1247288e9
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-08-24 23:49:25 +0800

    fix: nest routes for learner pages to learner prefix

--- routes/web.php
+++ routes/web.php
@@ -15,45 +15,50 @@ Route::get('/signup', function () {
     return view('signup');
 });
 
-Route::get('/learner/classes', function () {
-    return view('learner.classes');
-})->name('learner.classes');
 
-Route::get('/learner/dashboard', function () {
-    return view('learner.dashboard');
-})->name('learner.dashboard');
-
-Route::get('/learner/profile', function () {
-    return view('learner.profile');
-})->name('learner.profile');
-
-Route::get('/learner/enrolled', function () {
-    return view('learner.enrolled');
-})->name('learner.enrolled');
-
-Route::get('/learner/activity', function () {
-    return view('learner.activity');
-})->name('learner.activity');
-
-Route::get('/learner/course', function () {
-    return view('learner.course');
-})->name('learner.course');
-
-Route::get('/learner/activitytest', function () {
-    return view('learner.activitytest');
-})->name('learner.activitytest');
-
-Route::get('/learner/submission', function () {
-    return view('learner.submission');
-})->name('learner.submission');
-
-Route::get('/learner/assessment', function () {
-    return view('learner.assessment');
-})->name('learner.assessment');
-
-Route::get('/learner/evaluation', function () {
-    return view('learner.evaluation');
-})->name('learner.evaluation');
 
 
 // End //
+
+Route::prefix('learner')->group(function () {
+        // ADMIN DASHBOARD
+    Route::get('/classes', function () {
+        return view('learner.classes');
+    })->name('learner.classes');
+
+    Route::get('/dashboard', function () {
+        return view('learner.dashboard');
+    })->name('learner.dashboard');
+
+    Route::get('/profile', function () {
+        return view('learner.profile');
+    })->name('learner.profile');
+
+    Route::get('/enrolled', function () {
+        return view('learner.enrolled');
+    })->name('learner.enrolled');
+
+    Route::get('/activity', function () {
+        return view('learner.activity');
+    })->name('learner.activity');
+
+    Route::get('/course', function () {
+        return view('learner.course');
+    })->name('learner.course');
+
+    Route::get('/activitytest', function () {
+        return view('learner.activitytest');
+    })->name('learner.activitytest');
+
+    Route::get('/submission', function () {
+        return view('learner.submission');
+    })->name('learner.submission');
+
+    Route::get('/assessment', function () {
+        return view('learner.assessment');
+    })->name('learner.assessment');
+
+    Route::get('/evaluation', function () {
+        return view('learner.evaluation');
+    })->name('learner.evaluation');
+    });
