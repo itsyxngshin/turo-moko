e1a380982d4478fce4e55af345766ba8652629fa
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-08-24 17:41:31 +0800

    chore: update lock file, css, and js for sweetalert2 and tailwind

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
