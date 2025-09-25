b38370fe2c442f5009924b45058cb63fa743ce11
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-09-25 19:25:38 +0800

    chore: populate seeder files for authentication process

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('roles')->insert([
+            ['role_name' => 'learner', 'description' => 'Regular user with limited access', 'created_at' => now(), 'updated_at' => now()],
+            ['role_name' => 'implementer', 'description' => 'User with permissions to create course', 'created_at' => now(), 'updated_at' => now()],
+            ['role_name' => 'admin', 'description' => 'Administrator with full access', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,40 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            [
+                'first_name' => 'Jane',
+                'last_name' => 'Doe',
+                'username' => 'jane_doe',
+                'email' => 'janedoe@turo-moko.com',
+                'phonenum' => '+639123456789',
+                'password' => Hash::make('admin123'),
+                'role_id' => 1,
+                'profile_id' => null,
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        DB::table('users')->insert($users);
+        $users = [
+        [
+            'first_name' => 'DoughMain',
+            'last_name' => 'Admin',
+            'username' => 'doughmain_official',
+            'email' => 'jane.doe@',
+            'password' => bcrypt('admin123'),
+            'phone_number' => '+639123456789',
+            'profile_photo' => '1000071957-01.jpg',
+            'role_id' => 3,
+            'user_status_id' => 1,
+            'nationality' => 'Filipino',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ],
+    ];
+
+    DB::table('users')->insert($users);
     }
 }
