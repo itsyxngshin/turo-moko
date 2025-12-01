b1d20aebc1d7de0561f6a71e7308495fc7d4110f
narisan21 <dmor2022-9409-34709@bicol-u.edu.ph>
2025-12-01 19:19:34 +0800

    minor fix

--- database/seeders/SubcategorySeeder.php
+++ database/seeders/SubcategorySeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Subcategory;
 use Illuminate\Database\Seeder;
 
-class SubcategorySeeder extends Seeder
+class SubCategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $category = Category::first(); // make sure at least 1 category exists
+
+        Subcategory::create([
+            'category_id' => $category->id,
+            'subcategory_name' => 'Frontend Development',
+            'description' => 'Covers HTML, CSS, and JavaScript',
+        ]);
     }
 }
+
