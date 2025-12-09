067fe03dea22cf2d30dac93a0b3bf832982d81e3
narisan21 <dmor2022-9409-34709@bicol-u.edu.ph>
2025-12-09 21:55:29 +0800

    added x-cloak on navbar for mobile

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Turo-Moko Login</title>
-  <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
   <link rel="icon" href="{{ asset('images/turo_moko_logo.png') }}" type="image/png">
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,3 @@
     <livewire:landing-navbar />
     <livewire:welcome />
 @endsection
-
-<!-- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>TuroMoko</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="//unpkg.com/alpinejs" defer></script>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-  <link rel="icon" href="{{ asset('images/turo_moko_logo.png') }}" type="image/png">
-  <style>
-    body {
-      font-family: 'Poppins', sans-serif;
-    }
-    html {
-    scroll-behavior: smooth;
-    }
-  </style>
-</head>
-
-</html>
---> 
