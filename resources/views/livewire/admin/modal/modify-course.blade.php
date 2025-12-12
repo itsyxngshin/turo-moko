723fbe27b6f29e3a713b08ea851188fb408f00b7
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-12-13 02:39:36 +0800

    fix: add icon for modify

--- resources/views/livewire/admin/modal/modify-course.blade.php
+++ resources/views/livewire/admin/modal/modify-course.blade.php
@@ -2,8 +2,10 @@
     <!-- Trigger button -->
     <button 
         @click="open = true"
-        class="text-orange-500 px-4 py-2 rounded-lg hover:underline">
-        Edit
+       class="text-gray-500 hover:text-blue-600 transition">
+        <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+        </svg>
     </button>
 
     <!-- Modal Background -->
